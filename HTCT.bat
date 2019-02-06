f878d8fadd69f21f0bac9f669be5f7f66f9587c9
leonard-psu <lnl3@psu.edu>
2019-02-06 15:16:07 -0500

    Added Windows .bat script
    Updated Help to reflect windows script

---
+++ HTCT.bat
@@ -0,0 +1,50 @@
+@echo off 
+REM ====================================================================================================================================================
+set indir=E:\Temp\HydroTerre_ETV_Data
+set outdir=%indir%\PIHM
+set java_location="C:\Program Files\Java\jre1.8.0_201\bin\java"
+set HTCT_location="E:\PIHM_GIS\git_base\HydroTerre2PIHM.Java\HTCT.jar"
+
+REM ====================================================================================================================================================
+
+set message=Starting HTCT tool
+echo %message%
+echo INDIR=%indir%
+echo OUTDIR=%outdir%
+
+set nodatavalue=255
+set ext=asc
+set forcmap=HT_ForcingIDs.shp
+set forctif=HT_ForcingIDs.tif
+set forcasc=HT_ForcingIDs.asc
+set gsgmap=HT_GGSURGO.shp
+set gsgtif=HT_GGSURGO.tif
+set gsgasc=HT_GGSURGO.asc
+
+REM ====================================================================================================================================================
+mkdir %outdir%
+
+echo Rasterizing Forcing map: %forcmap%
+
+gdal_rasterize -a Force_ID  -a_nodata %nodatavalue% -ot Int32 -l HT_ForcingIDs -tr 30 30 %indir%\%forcmap% %indir%\%forctif%
+gdal_translate -of AAIGrid %indir%\%forctif% %outdir%\%forcasc%
+gdal_rasterize -a MUKEY -a_nodata $nodatavalue -ot Int32 -l HT_GGSURGO -tr 30 30 %indir%\%gsgmap% %indir%\%gsgtif%
+gdal_translate -of AAIGrid %indir%\%gsgtif% %outdir%\%gsgasc%
+
+REM ====================================================================================================================================================
+REM Need to use ! for variables not % Classic problem
+setlocal enableDelayedExpansion
+FOR %%i IN (%indir%\*.tif) DO (
+ set name=%%i
+ echo !name!
+ set str=!name:%indir%\=!
+ set str=!str:.tif=.asc!
+ set outputfname=!outdir!\!str!
+ echo !str!
+ echo !outputfname!
+ gdal_translate -of AAIGrid %%i !outputfname!
+ )
+
+REM ====================================================================================================================================================
+
+%java_location% -jar -Xmx8048m -Xms8012m %HTCT_location% %indir%\ %outdir%\
--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-# HydroTerre2PIHM
-Convert the Data from HydroTerre ETV to hydrologic models, such as PIHM.
+### HydroTerre2PIHM (HTCT) 
+Convert HydroTerre ETV data bundles to be ready for PIHMgis v3.5 and PIHM v2.2.
+
+### Windows Users
+Edit and use HTCT.bat
+
+Windows users need to edit the following lines (top portion of HTCT.bat file)
+1. Specify the input folder location of the unzipped ETV data bundle    set indir=E:\Temp\HydroTerre_ETV_Data
+2. Specify the output folder location                                   set outdir=%indir%\PIHM
+3. Specify the java install location                                    set java_location="C:\Program Files\Java\jre1.8.0_201\bin\java"
+4. Specify the HTCT.jar location (from this git site)                   set HTCT_location="E:\PIHM_GIS\git_base\HydroTerre2PIHM.Java\HTCT.jar"
+
+### Windows Software Dependencies
+1. JAVA JRE needs to be installed. The HTCT.bat has been tested with jre1.8.0_201
+2. GDAL tools [gdal_rasterize, gdal_translate, and plugins] are required. Use of these methods:
+2.1 Install and build by source code https://www.gdal.org/
+2.2 OSGeo4W installer https://trac.osgeo.org/osgeo4w/ [Note: Tested with installing gdal components only]
+
+### Linux and Mac Users
+Use HTCT.sh
+
+### Linux and Mac Software Dependencies
+1. JAVA JRE needs to be installed.
+2. GDAL
 
 Contact Lele Shu (lele 'dot' shu 'at' gmail.com) for more help.
 
